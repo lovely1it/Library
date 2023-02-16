c3d4d92c0f4993d2a5791ebc8ff3d908f99b2703
Lovely123Mastery <106240801+Lovely123Mastery@users.noreply.github.com>
2023-02-16 10:47:26 +0530

    added new line

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const express = require('express');
-const chalk = require('chalk');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use( express.static( path.join(__dirname, 'public')));
-app.use('/css', express.static( path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static( path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static( path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views','index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.listen(3000, () => {
-    console.log(`app is listening at ${chalk.green(3000)}`);
+  console.log(`app is listening at ${chalk.green(3000)}`);
 });
+console.log("Today is brand new day!");
